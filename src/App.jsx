1c90a07d112e378186bf8c7a73dc10bee7ecf9dc
aravindm18092006 <aravindm180906@gmail.com>
2026-06-09 10:22:19 +0530

    Hide navbar and footer on login and signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './Styles/theme';
 import AppRoutes from './Routes/AppRoutes';
@@ -8,6 +8,20 @@ import { WishlistProvider } from './Context/WishlistContext';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 
+const hideNavFooterRoutes = ['/login', '/signup'];
+
+function Layout() {
+  const { pathname } = useLocation();
+  const hide = hideNavFooterRoutes.includes(pathname);
+  return (
+    <>
+      {!hide && <Navbar />}
+      <AppRoutes />
+      {!hide && <Footer />}
+    </>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -15,9 +29,7 @@ function App() {
       <CartProvider>
         <WishlistProvider>
           <BrowserRouter>
-            <Navbar />
-            <AppRoutes />
-            <Footer />
+            <Layout />
           </BrowserRouter>
         </WishlistProvider>
       </CartProvider>
